e5bf9c3fb8044b00c977089775e83373485d6f6b
Geessyca <72661229+Geessyca@users.noreply.github.com>
2021-01-11 13:54:34 -0300

    Update code.py

--- code.py
+++ code.py
@@ -24,9 +24,10 @@ failures.isnull().sum()
 machines.isnull().sum()
 
 #substituindo pelo valor  - NÃO TEVE NECESSIDADE
-    #voltmean = telemetry['volt'].mean()
-    #voltmean = math.floor (voltmean)
-    #telemetry.update(telemetry['volt'].fillna(voltmean))
+    #dadomedio = df['dado'].mean()
+    #dadomedio = math.floor (dadomedio)
+
+    #df.update(df['dado'].fillna(dadomedio))
     
 #Telemetry gráficos 1º ano
 
@@ -129,7 +130,6 @@ maint['comp'].value_counts().plot(kind='bar')
 plt.ylabel('Nº de componentes')
 
 #Recursos de atraso de 3 horas
-
 temp = []
 fields = ['volt', 'rotate', 'pressure', 'vibration']
 for col in fields:
@@ -190,3 +190,220 @@ telemetrysd_24hrs = pd.concat(temp, axis=1)
 telemetrysd_24hrs.columns = [i + 'sd_24h' for i in fields]
 telemetrysd_24hrs.reset_index(inplace=True)
 telemetrysd_24hrs = telemetrysd_24hrs.loc[-telemetrysd_24hrs['voltsd_24h'].isnull()]
+
+#Mesclar os recursos anteriores
+telemetry_feat  =  pd.concat ([ telemetrymean ,
+                             telemetrysd.iloc [:, 2 : 6 ],
+                             telemetrymean_24hrs.iloc [:, 2 : 6 ],
+                             telemetrysd_24hrs.iloc [:, 2 : 6 ]], axis = 1 ).dropna ()
+
+#Recursos de atraso de errors
+error_count = pd.get_dummies(errors.set_index('datetime')).reset_index()
+error_count.columns = ['datetime', 'machineID', 'error1', 'error2', 'error3', 'error4', 'error5']
+error_count = error_count.groupby(['machineID','datetime']).sum().reset_index()
+error_count = telemetry[['datetime', 'machineID']].merge(error_count, on=['machineID', 'datetime'], how='left').fillna(0.0)
+
+#Número total de erros de cada tipo nas últimas 24 horas
+temp = []
+fields = ['error%d' % i for i in range(1,6)]
+for col in fields:
+    temp.append(pd.pivot_table(error_count.rolling(window=24,center=False).sum()).resample('3H',
+                                                                                           closed='left',
+                                                                                           label='right',
+                                                                                           how='first').first())
+error_count = pd.concat(temp, axis=1)
+error_count.columns = [i + 'count' for i in fields]
+error_count.reset_index(inplace=True)
+error_count = error_count.dropna()
+error_count.describe()
+
+
+
+#Dias desde a última substituição da manutenção
+import numpy as np
+
+# crie uma coluna para cada tipo de erro 
+
+comp_rep = pd.get_dummies(maint.set_index('datetime')).reset_index()
+comp_rep.columns = ['datetime', 'machineID', 'comp1', 'comp2', 'comp3', 'comp4']
+
+# combina reparos para uma determinada máquina em uma determinada hora 
+comp_rep = comp_rep.groupby(['machineID', 'datetime']).sum().reset_index()
+
+# adicionar pontos de tempo onde nenhum componente foi substituído 
+comp_rep = telemetry[['datetime', 'machineID']].merge(comp_rep,
+                                                      on=['datetime', 'machineID'],
+                                                      how='outer').fillna(0).sort_values(by=['machineID', 'datetime'])
+
+components = ['comp1', 'comp2', 'comp3', 'comp4']
+for comp in components:
+     # converter o indicador para a data mais recente de alteração do componente 
+    comp_rep.loc[comp_rep[comp] < 1, comp] = None
+    comp_rep.loc[-comp_rep[comp].isnull(), comp] = comp_rep.loc[-comp_rep[comp].isnull(), 'datetime']
+    
+    # forward-fill a data mais recente da alteração do componente 
+    comp_rep[comp] = comp_rep[comp].fillna(method='ffill')
+
+# remove as datas em 2014 (pode ter NaN ou datas de mudança de componente futura)     
+comp_rep = comp_rep.loc[comp_rep['datetime'] > pd.to_datetime('2015-01-01')]
+
+# substitua as datas da mudança de componente mais recente por dias desde a mudança de componente mais recente 
+for comp in components:
+    comp_rep[comp] = (comp_rep["datetime"] - pd.to_datetime(comp_rep[comp])) / np.timedelta64(1, "D") 
+
+#Características da máquina
+final_feat = telemetry_feat.merge(error_count, on=['datetime', 'machineID'], how='left')
+final_feat = final_feat.merge(comp_rep, on=['datetime', 'machineID'], how='left')
+final_feat = final_feat.merge(machines, on=['machineID'], how='left')
+
+#Construção de etiqueta
+labeled_features = final_feat.merge(failures, on=['datetime', 'machineID'], how='left')
+labeled_features = labeled_features.fillna(method='bfill', limit=7) # fill backward up to 24h
+labeled_features = labeled_features.fillna('none')
+
+#Modelagem
+#Treinamento, Validação e Teste
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+# fazer divisões de teste e treinamento 
+threshold_dates = [[pd.to_datetime('2015-07-31 01:00:00'), pd.to_datetime('2015-08-01 01:00:00')],
+                   [pd.to_datetime('2015-08-31 01:00:00'), pd.to_datetime('2015-09-01 01:00:00')],
+                   [pd.to_datetime('2015-09-30 01:00:00'), pd.to_datetime('2015-10-01 01:00:00')]]
+
+test_results = []
+models = []
+for last_train_date, first_test_date in threshold_dates:
+     # divide os dados de treinamento e teste 
+    train_y = labeled_features.loc[labeled_features['datetime'] < last_train_date, 'failure']
+    train_X = pd.get_dummies(labeled_features.loc[labeled_features['datetime'] < last_train_date].drop(['datetime',
+                                                                                                        'machineID',
+                                                                                                        'failure'], 1))
+    test_X = pd.get_dummies(labeled_features.loc[labeled_features['datetime'] > first_test_date].drop(['datetime',
+                                                                                                       'machineID',
+                                                                                                       'failure'], 1))
+     # treinar e prever usando o modelo, armazenando resultados para posterior 
+    my_model = GradientBoostingClassifier(random_state=42)
+    my_model.fit(train_X, train_y)
+    test_result = pd.DataFrame(labeled_features.loc[labeled_features['datetime'] > first_test_date])
+    test_result['predicted_failure'] = my_model.predict(test_X)
+    test_results.append(test_result)
+    models.append(my_model)
+    
+sns.set_style("darkgrid")
+plt.figure(figsize=(10, 6))
+labels, importances = zip(*sorted(zip(test_X.columns, models[0].feature_importances_), reverse=True, key=lambda x: x[1]))
+plt.xticks(range(len(labels)), labels)
+_, labels = plt.xticks()
+plt.setp(labels, rotation=90)
+plt.bar(range(len(importances)), importances)
+plt.ylabel('Importance')
+
+#Avaliação
+
+sns.set_style("darkgrid")
+plt.figure(figsize=(8, 4))
+labeled_features['failure'].value_counts().plot(kind='bar')
+plt.xlabel('Component failing')
+plt.ylabel('Count')
+
+from sklearn.metrics import confusion_matrix, recall_score, accuracy_score, precision_score
+
+def Evaluate(predicted, actual, labels):
+    output_labels = []
+    output = []
+    
+    # Calcular e exibir a matriz de confusão 
+    cm = confusion_matrix(actual, predicted, labels=labels)
+    print('Confusion matrix\n- x-axis is true labels (none, comp1, etc.)\n- y-axis is predicted labels')
+    print(cm)
+    
+    # Calcule precisão, recall e precisão de  pontuação F1
+    accuracy = np.array([float(np.trace(cm)) / np.sum(cm)] * len(labels))
+    precision = precision_score(actual, predicted, average=None, labels=labels)
+    recall = recall_score(actual, predicted, average=None, labels=labels)
+    f1 = 2 * precision * recall / (precision + recall)
+    output.extend([accuracy.tolist(), precision.tolist(), recall.tolist(), f1.tolist()])
+    output_labels.extend(['accuracy', 'precision', 'recall', 'F1'])
+    
+    # Calcule as versões macro da saída dessas métricas
+    output.extend([[np.mean(precision)] * len(labels),
+                   [np.mean(recall)] * len(labels),
+                   [np.mean(f1)] * len(labels)])
+    output_labels.extend(['macro precision', 'macro recall', 'macro F1'])
+    
+    # Encontre a matriz de confusão um x todos 
+    cm_row_sums = cm.sum(axis = 1)
+    cm_col_sums = cm.sum(axis = 0)
+    s = np.zeros((2, 2))
+    for i in range(len(labels)):
+        v = np.array([[cm[i, i],
+                       cm_row_sums[i] - cm[i, i]],
+                      [cm_col_sums[i] - cm[i, i],
+                       np.sum(cm) + cm[i, i] - (cm_row_sums[i] + cm_col_sums[i])]])
+        s += v
+    s_row_sums = s.sum(axis = 1)
+    
+    # Adicionar precisão média e precisão / recall / F1 
+    avg_accuracy = [np.trace(s) / np.sum(s)] * len(labels)
+    micro_prf = [float(s[0,0]) / s_row_sums[0]] * len(labels)
+    output.extend([avg_accuracy, micro_prf])
+    output_labels.extend(['average accuracy',
+                          'micro-averaged precision/recall/F1'])
+    
+    # Calcule métricas para o classificador majoritário 
+    mc_index = np.where(cm_row_sums == np.max(cm_row_sums))[0][0]
+    cm_row_dist = cm_row_sums / float(np.sum(cm))
+    mc_accuracy = 0 * cm_row_dist; mc_accuracy[mc_index] = cm_row_dist[mc_index]
+    mc_recall = 0 * cm_row_dist; mc_recall[mc_index] = 1
+    mc_precision = 0 * cm_row_dist
+    mc_precision[mc_index] = cm_row_dist[mc_index]
+    mc_F1 = 0 * cm_row_dist;
+    mc_F1[mc_index] = 2 * mc_precision[mc_index] / (mc_precision[mc_index] + 1)
+    output.extend([mc_accuracy.tolist(), mc_recall.tolist(),
+                   mc_precision.tolist(), mc_F1.tolist()])
+    output_labels.extend(['majority class accuracy', 'majority class recall',
+                          'majority class precision', 'majority class F1'])
+        
+    # Precisão aleatória e kappa 
+    cm_col_dist = cm_col_sums / float(np.sum(cm))
+    exp_accuracy = np.array([np.sum(cm_row_dist * cm_col_dist)] * len(labels))
+    kappa = (accuracy - exp_accuracy) / (1 - exp_accuracy)
+    output.extend([exp_accuracy.tolist(), kappa.tolist()])
+    output_labels.extend(['expected accuracy', 'kappa'])
+    
+
+    # Estimativa  aleatória
+    rg_accuracy = np.ones(len(labels)) / float(len(labels))
+    rg_precision = cm_row_dist
+    rg_recall = np.ones(len(labels)) / float(len(labels))
+    rg_F1 = 2 * cm_row_dist / (len(labels) * cm_row_dist + 1)
+    output.extend([rg_accuracy.tolist(), rg_precision.tolist(),
+                   rg_recall.tolist(), rg_F1.tolist()])
+    output_labels.extend(['random guess accuracy', 'random guess precision',
+                          'random guess recall', 'random guess F1'])
+    
+    # Estimativa  ponderada
+    rwg_accuracy = np.ones(len(labels)) * sum(cm_row_dist**2)
+    rwg_precision = cm_row_dist
+    rwg_recall = cm_row_dist
+    rwg_F1 = cm_row_dist
+    output.extend([rwg_accuracy.tolist(), rwg_precision.tolist(),
+                   rwg_recall.tolist(), rwg_F1.tolist()])
+    output_labels.extend(['random weighted guess accuracy',
+                          'random weighted guess precision',
+                          'random weighted guess recall',
+                          'random weighted guess F1'])
+
+    output_df = pd.DataFrame(output, columns=labels)
+    output_df.index = output_labels
+                  
+    return output_df
+
+evaluation_results = []
+for i, test_result in enumerate(test_results):
+    print('\nSplit %d:' % (i+1))
+    evaluation_result = Evaluate(actual = test_result['failure'],
+                                 predicted = test_result['predicted_failure'],
+                                 labels = ['none', 'comp1', 'comp2', 'comp3', 'comp4'])
+    evaluation_results.append(evaluation_result)
